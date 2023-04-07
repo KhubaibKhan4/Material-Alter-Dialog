1dfa935532ce1b7c2faead01c999a07710aef103
Khubaib Khan <18.bscs.803@gmail.com>
2023-04-08 01:20:10 +0500

    Material Alert Dialog Android

---
+++ app/src/main/java/com/example/portofolioapp/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.portofolioapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.afollestad.materialdialogs.MaterialDialog;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"})
+        Button clickme = (Button) findViewById(R.id.clickBtn);
+
+        clickme.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(MainActivity.this);
+                builder.setTitle("This is Material Dialog")
+                        .setMessage("Material Alert Dialog is a Best Way to Display Alert Dialogs in Android")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                Toast.makeText(MainActivity.this, "Yes Button Clicked", Toast.LENGTH_SHORT).show();
+                            }
+                        }).setNegativeButton("NO", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                Toast.makeText(MainActivity.this, "No Button Clicked", Toast.LENGTH_SHORT).show();
+                            }
+                        }).setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                Toast.makeText(MainActivity.this, "Cancle Button Clicked", Toast.LENGTH_SHORT).show();
+                            }
+                        }).show();
+            }
+        });
+
+
+    }
+}
